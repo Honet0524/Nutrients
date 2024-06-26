e79c34cac915139d871a42d78d80fc5a4839a1e6
Honet0524 <cus0524@gmail.com>
2024-06-26 16:11:30 +0900

    upload

---
+++ src/main/java/com/project/Nutrients/NutrientsApplication.java
@@ -0,0 +1,13 @@
+package com.project.Nutrients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/Nutrients/NutrientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Nutrients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
